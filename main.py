329c47e41b9d9319ab9999021811f390c6f03a18
Junseo Kim <jookpower1022@gmail.com>
2025-06-15 14:17:51 +0900

    영상나오게

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from utils.alsa_suppress import suppress_alsa_errors
 from routers.ws_router import router as ws_router
 from routers.ws_audio_receive import router as audio_receive_router
+from routers.mjpeg_router import router as mjpeg_router
 from routers.ws_audio_send import router as audio_send_router
 from services.microphone_sender_instance import mic_streamer  # ✅ 싱글톤 마이크 객체 import
 
@@ -10,6 +11,7 @@ app = FastAPI()
 app.include_router(ws_router)
 app.include_router(audio_receive_router)
 app.include_router(audio_send_router)
+app.include_router(mjpeg_router)
 
 @app.on_event("startup")
 async def startup_event():
