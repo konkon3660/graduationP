30ea41a42def9f07159b74238a04406b85c322e7
Junseo Kim <jookpower1022@gmail.com>
2025-06-22 18:01:10 +0900

    emotino(index.html)

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.mount("/static", StaticFiles(directory="."), name="static")
 
 @app.get("/")
 async def root():
-    return HTMLResponse(content=open("test_json_commands.html", "r", encoding="utf-8").read())
+    return HTMLResponse(content=open("index.html", "r", encoding="utf-8").read())
 
 
 # ✅ 라우터 등록
